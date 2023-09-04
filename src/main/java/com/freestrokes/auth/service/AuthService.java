b07c615c2344640651a68b450b0cd4f5afd43589
freestrokes <freestrokes.hk@gmail.com>
2023-09-04 19:17:54 +0900

    [main] feat: AuthService.java 주석 수정

--- src/main/java/com/freestrokes/auth/service/AuthService.java
+++ src/main/java/com/freestrokes/auth/service/AuthService.java
@@ -32,10 +32,10 @@ public class AuthService implements AuthRequestService {
 
     /**
      * 로그인
-     *
-     * @param loginRequestDto
-     * @return
-     * @throws Exception
+     * @param loginRequestDto 로그인 정보
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @return 로그인 인증 정보
      */
     @Transactional
     public ResponseEntity<?> login(
