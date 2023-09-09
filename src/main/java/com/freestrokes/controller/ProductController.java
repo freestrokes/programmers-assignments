defc0710126e759253eba707695c4d899fb4f85d
freestrokes <freestrokes.hk@gmail.com>
2023-09-09 22:10:27 +0900

    [main] feat: ProductController.java 수정

--- src/main/java/com/freestrokes/controller/ProductController.java
+++ src/main/java/com/freestrokes/controller/ProductController.java
@@ -1,54 +1,58 @@
 package com.freestrokes.controller;
 
+import com.freestrokes.aop.LogExecutionTime;
 import com.freestrokes.properties.ApplicationProperties;
+import com.freestrokes.constants.PathConstants;
+import com.freestrokes.dto.BoardDto;
+import com.freestrokes.service.BoardService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springdoc.api.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 
-// TODO: @RequestMapping
-// 해당 컨트롤러 하위 전체 메서드에 공통 path 설정이 필요한 경우 class 상단에 어노테이션 사용.
-// @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 @RestController
 public class ProductController {
 
-    // TODO: ApplicationProperties 테스트를 위해 추가
-    private final ApplicationProperties applicationProperties;
-
 //    private final BoardService boardService;
+//    private final ProductService productService;
 
-    // TODO: DI(Dependency Injection)에 대한 설명
-    // 일반적으로 아래 코드처럼 생성자를 통해 의존성 주입을 설정해줌.
-    // 스프링 IoC Container가 해당 의존성 타입에 맞는 bean을 만들어서 주입 (의존성 관리)
-    // 의존성 주입에는 생성자, 필드 + setter 2가지 방법이 있는데
-    // 레퍼런스에서는 생성자를 이용한 방법을 권장 (생성자는 lombok의 @RequiredArgsConstructor를 사용해서 생략 가능)
-    // 필드 + setter를 사용한 경우엔 의존성 주입 없이 인스턴스 생성이 가능하다는 문제가 있음. (이 경우엔 @Autowired 어노테이션을 사용해야 함)
-    // 생성자를 사용한 경우엔 순환 참조가 발생할 수 있음.
-
-    // TODO: 생성자를 이용한 의존성 주입(DI) 예시.
-//    public BoardController(ApplicationProperties applicationProperties, BoardService boardService) {
-//        this.applicationProperties = applicationProperties;
-//        this.boardService = boardService;
-//    }
-
-//    @GetMapping(path = PathConstants.BOARDS, produces = "application/json")
-//    @Operation(
-//        summary = "게시글 목록 조회",
-//        description = "페이징을 이용하여 게시글 목록을 조회한다."
-//    )
-//    // TODO: AOP 확인을 위해 추가 (@LogExecutionTime)
-//    @LogExecutionTime
-//    public ResponseEntity<Page<BoardDto.ResponseDto>> getBoards(
-//        // TODO: size, sort, direction 프로퍼티를 설정한 @PageableDefault 예시
-////        @ParameterObject @PageableDefault(size = 10, sort = "modifiedAt", direction = Sort.Direction.DESC) Pageable pageable
-//        @ParameterObject @PageableDefault(size = 10) Pageable pageable
-//    ) throws Exception {
-//
-//        // TODO: ApplicationProperties 테스트를 위해 추가
-//        System.out.println("applicationProperties: " + applicationProperties);
-//
+    @GetMapping(path = PathConstants.PRODUCTS, produces = "application/json")
+    @Operation(
+        summary = "상품 목록 조회",
+        description = "상품 목록을 조회한다."
+    )
+    public ResponseEntity<Page<BoardDto.ResponseDto>> getProducts(
+        // TODO: size, sort, direction 프로퍼티를 설정한 @PageableDefault 예시
+//        @ParameterObject @PageableDefault(size = 10, sort = "modifiedAt", direction = Sort.Direction.DESC) Pageable pageable
+        @ParameterObject @PageableDefault(size = 10) Pageable pageable
+    ) throws Exception {
+        // TODO
 //        Page<BoardDto.ResponseDto> result = boardService.getBoards(pageable);
-//        return new ResponseEntity<Page<BoardDto.ResponseDto>>(result, HttpStatus.OK);
-//    }
+        Page<BoardDto.ResponseDto> result = null;
+        return new ResponseEntity<Page<BoardDto.ResponseDto>>(result, HttpStatus.OK);
+    }
+
+    @GetMapping(path = PathConstants.PRODUCTS_DETAIL, produces = "application/json")
+    @Operation(
+        summary = "단일 상품 조회",
+        description = "단일 상품 정보를 조회한다."
+    )
+    public ResponseEntity<BoardDto.ResponseDto> getProductDetail(
+        @PathVariable("productId") String productId
+    ) throws Exception {
+        // TODO
+//        BoardDto.ResponseDto result = boardService.getBoardDetail(productId);
+        BoardDto.ResponseDto result = null;
+        return new ResponseEntity<BoardDto.ResponseDto>(result, HttpStatus.OK);
+    }
+
 //
 //    @PostMapping(path = PathConstants.BOARDS, produces = "application/json")
 //    @Operation(
