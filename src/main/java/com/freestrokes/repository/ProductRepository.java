bd4c5bd3ca31bbeb98c3fd5d835c6e51795ec15c
freestrokes <freestrokes.hk@gmail.com>
2023-09-13 21:57:58 +0900

    [main] feat: Order, Product, Review repository 추가

---
+++ src/main/java/com/freestrokes/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.freestrokes.repository;
+
+import com.freestrokes.domain.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, String> {
+
+    Page<Product> findAll(Pageable pageable);
+
+}
