3cac9c5b7e34ad7e8326427f968f119b14d75f0d
freestrokes <freestrokes.hk@gmail.com>
2023-09-14 21:05:18 +0900

    [main] feat: Product 컨트롤러 수정

--- src/main/java/com/freestrokes/controller/ProductController.java
+++ src/main/java/com/freestrokes/controller/ProductController.java
@@ -1,10 +1,7 @@
 package com.freestrokes.controller;
 
-import com.freestrokes.aop.LogExecutionTime;
-import com.freestrokes.properties.ApplicationProperties;
 import com.freestrokes.constants.PathConstants;
-import com.freestrokes.dto.BoardDto;
-import com.freestrokes.service.BoardService;
+import com.freestrokes.dto.ProductDto;
 import com.freestrokes.service.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 
 @RequiredArgsConstructor
@@ -28,13 +24,13 @@ public class ProductController {
         summary = "상품 목록 조회",
         description = "상품 목록을 조회한다."
     )
-    public ResponseEntity<Page<BoardDto.ResponseDto>> getProducts(
+    public ResponseEntity<Page<ProductDto.ResponseDto>> getProducts(
         // TODO: size, sort, direction 프로퍼티를 설정한 @PageableDefault 예시
 //        @ParameterObject @PageableDefault(size = 10, sort = "modifiedAt", direction = Sort.Direction.DESC) Pageable pageable
         @ParameterObject @PageableDefault(size = 10) Pageable pageable
     ) throws Exception {
-        Page<BoardDto.ResponseDto> result = productService.getProducts(pageable);
-        return new ResponseEntity<Page<BoardDto.ResponseDto>>(result, HttpStatus.OK);
+        Page<ProductDto.ResponseDto> result = productService.getProducts(pageable);
+        return new ResponseEntity<Page<ProductDto.ResponseDto>>(result, HttpStatus.OK);
     }
 
     @GetMapping(path = PathConstants.PRODUCTS_DETAIL, produces = "application/json")
@@ -42,12 +38,12 @@ public class ProductController {
         summary = "단일 상품 조회",
         description = "단일 상품 정보를 조회한다."
     )
-    public ResponseEntity<BoardDto.ResponseDto> getProductDetail(
+    public ResponseEntity<ProductDto.ResponseDto> getProductDetail(
         @PathVariable("productId") String productId
     ) throws Exception {
         // TODO
-        BoardDto.ResponseDto result = productService.getProductDetail(productId);
-        return new ResponseEntity<BoardDto.ResponseDto>(result, HttpStatus.OK);
+        ProductDto.ResponseDto result = productService.getProductDetail(productId);
+        return new ResponseEntity<ProductDto.ResponseDto>(result, HttpStatus.OK);
     }
 
 }
