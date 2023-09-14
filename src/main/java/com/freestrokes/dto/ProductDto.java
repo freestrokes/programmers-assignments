0764cd37081d9ab506e001739a0f9b832f2c7693
freestrokes <freestrokes.hk@gmail.com>
2023-09-14 21:04:32 +0900

    [main] feat: Product DTO 수정

--- src/main/java/com/freestrokes/dto/ProductDto.java
+++ src/main/java/com/freestrokes/dto/ProductDto.java
@@ -1,11 +1,12 @@
 package com.freestrokes.dto;
 
-import com.freestrokes.domain.Board;
 import com.freestrokes.domain.BoardComment;
+import com.freestrokes.domain.Product;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -20,36 +21,37 @@ public class ProductDto {
 
     @Getter
     public static class RequestDto {
-        private String title;
-        private String content;
-        private String author;
+        private String name;
+        private String details;
+        private Integer reviewCount;
 
         @Builder(toBuilder = true)
         public RequestDto(
-            String title,
-            String content,
-            String author
+            String name,
+            String details,
+            Integer reviewCount
         ) {
-            this.title = title;
-            this.content = content;
-            this.author = author;
+            this.name = name;
+            this.details = details;
+            this.reviewCount = reviewCount;
         }
 
-        public Board toEntity() {
-            return Board.builder()
-                .title(title)
-                .content(content)
-                .author(author)
+        public Product toEntity() {
+            return Product.builder()
+                .name(name)
+                .details(details)
+                .reviewCount(reviewCount)
                 .build();
         }
     }
 
     @Getter
     public static class ResponseDto {
-        private String boardId;
-        private String title;
-        private String content;
-        private String author;
+        private String productId;
+        private String name;
+        private String details;
+        private Integer reviewCount;
+        private LocalDateTime createAt;
 
         // TODO: @JsonIgnore
         // 양방향 연관관계 매핑을 한 경우 순환 참조가 발생할 수 있음
@@ -59,17 +61,17 @@ public class ProductDto {
 
         @Builder(toBuilder = true)
         public ResponseDto(
-            String boardId,
-            String title,
-            String content,
-            String author,
-            List<BoardComment> boardComments
+            String productId,
+            String name,
+            String details,
+            Integer reviewCount,
+            LocalDateTime createAt
         ) {
-            this.boardId = boardId;
-            this.title = title;
-            this.content = content;
-            this.author = author;
-            this.boardComments = boardComments;
+            this.productId = productId;
+            this.name = name;
+            this.details = details;
+            this.reviewCount = reviewCount;
+            this.createAt = createAt;
         }
     }
 
