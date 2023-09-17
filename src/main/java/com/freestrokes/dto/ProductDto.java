0d1adcfadf8526eed65cd56acb539f8b43705196
freestrokes <freestrokes.hk@gmail.com>
2023-09-17 20:27:00 +0900

    [main] feat: dto 수정

--- src/main/java/com/freestrokes/dto/ProductDto.java
+++ src/main/java/com/freestrokes/dto/ProductDto.java
@@ -13,12 +13,6 @@ import java.util.List;
 @NoArgsConstructor
 public class ProductDto {
 
-    // TODO: DTO 설명과 사용 목적
-    // Entity는 DB 테이블과 매핑되어 있기 때문에 request, response에 대한 DTO로 사용하면 안 됨.
-    // request, response에 대한 DTO는 별도로 만들어서 사용해야 함.
-    // View, DB 레이어별로 계층 분리를 철저하게 해주는 것이 권장 됨.
-    // DTO 클래스 내부에 static inner class를 이용하여 용도에 맞는 DTO를 생성하여 사용.
-
     @Getter
     public static class RequestDto {
         private String name;
